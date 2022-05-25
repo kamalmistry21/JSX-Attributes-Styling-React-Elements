a977b23d7a3044102d96c5dd3382141bbeda86cf
Kamal Mistry <31496227+kamalmistry21@users.noreply.github.com>
2022-05-25 22:26:32 +1200

    Adding css styling

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img = "https://picsum.photos/200";
+
 ReactDOM.render(
   <div>
     <h1 className="heading">My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <img alt="random" src={img} />
   </div>,
   document.getElementById("root")
 );
